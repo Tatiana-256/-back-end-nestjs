10bcb2323ce1f03c11876611cbf35088e036c1bb
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2021-05-21 17:24:04 +0300

    add controller, model, service for roles. Add user-roles table many-to-many

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,7 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
 import { ApiProperty } from "@nestjs/swagger";
+import { Role } from "../roles/roles.model";
+import { UserRoles } from "src/roles/user-roles.model";
 
 interface UserCreationAttrs {
   email: string,
@@ -8,19 +10,27 @@ interface UserCreationAttrs {
 
 @Table({ tableName: "users" })
 export class User extends Model<User, UserCreationAttrs> {
+
   @ApiProperty({example:"1", description: "uniq identification"})
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
   id: number;
+
   @ApiProperty({example:"user@gmail.com", description: "email"})
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
+
   @ApiProperty({example:"3j24f0g342", description: "password"})
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
+
   @ApiProperty({example:"false", description: "is baned"})
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
+
   @ApiProperty({example:"wrote bad words", description: "ban reason"})
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
+
+  @BelongsToMany(()=>Role, ()=>UserRoles)
+  roles: Role[]
 }
